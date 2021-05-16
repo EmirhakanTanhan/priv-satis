6ffbf0279b7483401f37c7bc55799419d31ffa52
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-05-16 16:29:33 +0300

    fixing missing files

--- _system/pdoconfig.php
+++ _system/pdoconfig.php
@@ -3,3 +3,7 @@
     $dbname = 'u567125998_Satis';
     $username = 'u567125998_emirhakan';
     $password = 'cE=9=9;hL6/';
+
+    /*$dbname = 'satis';
+    $username = 'root';
+    $password = '';*/
